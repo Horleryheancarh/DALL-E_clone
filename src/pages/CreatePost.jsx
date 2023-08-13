39c4c15556876d5c75ba8009984579b07d5c5626
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-08-13 16:31:43 +0100

    use env

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -5,6 +5,8 @@ import { preview } from '../assets'
 import { getRandomPrompt } from '../utils'
 import { FormField, Loader } from '../components'
 
+const apiKey = 'http://localhost:8080/api/v1';
+
 const CreatePost = () => {
   const navigate = useNavigate();
   const [form, setForm] = useState({
@@ -19,7 +21,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch(`${apiKey}/dalle`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -46,7 +48,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch(`${apiKey}/post`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
